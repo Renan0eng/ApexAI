6fc5d860df7e4752ebaed3728a56269b1c3956cc
Renan0eng <renan_terra_rica@hotmail.com.br>
2024-09-07 01:18:41 -0300

    chore: Update DATABASE_URL, SECRET_WEBHOOK_KEY, and OPENAI_API_KEY in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,9 @@ module.exports = {
       cwd: './', // Diretório de trabalho
       env: {
         NODE_ENV: 'production', // Ambiente de produção
-        DATABASE_URL: 'mysql://root:root@mysql:3306/apexai', // URL do banco de dados
+        DATABASE_URL: 'mysql://root:R3n@n159753@92.112.176.194:3306/apexai', // URL do banco de dados
+        SECRET_WEBHOOK_KEY: "segredo123",
+        OPENAI_API_KEY: "sk-uqFe7Ki9aLH9eiFGpR2f33Z4KQ2SQEoMy56qsiW5ujT3BlbkFJ1Sgjs3sdNwitKBZI9nz-wEWz-USkgyaGrage4wfFYA"
       },
     },
   ],
